f4be7933d8fe2b0b66ef2fed3f54f026ef918540
Jon Pesta <38979691+codingpierogi@users.noreply.github.com>
2023-08-05 14:12:25 -0400

    Initial binary search implementation.

---
+++ src/binary-tree.js
@@ -0,0 +1,65 @@
+export function TreeNode(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+}
+
+export function search(data, node) {
+    if (!node) {
+        return null;
+    } else if (node.data === data) {
+        return node;
+    } else if (data < node.data) {
+        return search(data, node.left);
+    } else {
+        return search(data, node.right);
+    }
+}
+
+export function insert(data, node) {
+    if (data < node.data) {
+        if (!node.left) {
+            node.left = new TreeNode(data);
+        } else {
+            insert(data, node.left);
+        }
+    } else {
+        if (!node.right) {
+            node.right = new TreeNode(data);
+        } else {
+            insert(data, node.right);
+        }
+    } 
+}
+
+export function remove(data, node) {
+    function lift(node, nodeToDelete) {
+        if (node.left) {
+            node.left = lift(node.left, nodeToDelete);
+            return node;
+        } else {
+            nodeToDelete.data = node.data;
+            return node.right;
+        }
+    }
+
+    if (!node) {
+        return null;
+    } else if (data < node.data) {
+        node.left = remove(data, node.left);
+        return node;
+    } else if (data > node.data) {
+        node.right = remove(data, node.right);
+        return node;
+    } else if (data === node.data) {
+        if (!node.left) {
+            return node.right;
+        } else if (!node.right) {
+            return node.left;
+        } else {
+            node.right = lift(node.right, node);
+            return node;
+        }
+    }
+}
+ 
---
+++ src/binary-tree.test.js
@@ -0,0 +1,182 @@
+import { test } from "tape";
+
+import { TreeNode, search, insert, remove } from "./binary-tree.js";
+
+test("binary tree should search", t => {
+    t.plan(1);
+
+    const node4 = new TreeNode(4);
+    const node11 = new TreeNode(11);
+    const node10 = new TreeNode(10, node4, node11);
+
+    const node30 = new TreeNode(30);
+    const node40 = new TreeNode(40);
+    const node33 = new TreeNode(33, node30, node40);
+
+    const node25 = new TreeNode(25, node10, node33);
+
+    const node52 = new TreeNode(52);
+    const node61 = new TreeNode(61);
+    const node56 = new TreeNode(56, node52, node61);
+
+    const node82 = new TreeNode(82);
+    const node95 = new TreeNode(95);
+    const node89 = new TreeNode(89, node82, node95);
+
+    const node75 = new TreeNode(75, node56, node89);
+
+    const root = new TreeNode(50, node25, node75);
+
+    t.equal(search(61, root), node61);
+});
+
+test("binary tree should search and not find", t => {
+    t.plan(1);
+
+    const node4 = new TreeNode(4);
+    const node11 = new TreeNode(11);
+    const node10 = new TreeNode(10, node4, node11);
+
+    const node30 = new TreeNode(30);
+    const node40 = new TreeNode(40);
+    const node33 = new TreeNode(33, node30, node40);
+
+    const node25 = new TreeNode(25, node10, node33);
+
+    const node52 = new TreeNode(52);
+    const node61 = new TreeNode(61);
+    const node56 = new TreeNode(56, node52, node61);
+
+    const node82 = new TreeNode(82);
+    const node95 = new TreeNode(95);
+    const node89 = new TreeNode(89, node82, node95);
+
+    const node75 = new TreeNode(75, node56, node89);
+
+    const root = new TreeNode(50, node25, node75);
+
+    t.notOk(search(100));
+});
+
+
+test("binary tree should insert", t => {
+    t.plan(1);
+
+    const node4 = new TreeNode(4);
+    const node11 = new TreeNode(11);
+    const node10 = new TreeNode(10, node4, node11);
+
+    const node30 = new TreeNode(30);
+    const node40 = new TreeNode(40);
+    const node33 = new TreeNode(33, node30, node40);
+
+    const node25 = new TreeNode(25, node10, node33);
+
+    const node52 = new TreeNode(52);
+    const node61 = new TreeNode(61);
+    const node56 = new TreeNode(56, node52, node61);
+
+    const node82 = new TreeNode(82);
+    const node95 = new TreeNode(95);
+    const node89 = new TreeNode(89, node82, node95);
+
+    const node75 = new TreeNode(75, node56, node89);
+
+    const root = new TreeNode(50, node25, node75);
+
+    insert(45, root);
+
+    t.ok(search(45, root));
+});
+
+test("binary tree should remove node with no children", t => {
+    t.plan(1);
+
+    const node4 = new TreeNode(4);
+    const node11 = new TreeNode(11);
+    const node10 = new TreeNode(10, node4, node11);
+
+    const node30 = new TreeNode(30);
+    const node40 = new TreeNode(40);
+    const node33 = new TreeNode(33, node30, node40);
+
+    const node25 = new TreeNode(25, node10, node33);
+
+    const node52 = new TreeNode(52);
+    const node61 = new TreeNode(61);
+    const node56 = new TreeNode(56, node52, node61);
+
+    const node82 = new TreeNode(82);
+    const node95 = new TreeNode(95);
+    const node89 = new TreeNode(89, node82, node95);
+
+    const node75 = new TreeNode(75, node56, node89);
+
+    const root = new TreeNode(50, node25, node75);
+
+    remove(4, root);
+
+    t.notOk(search(4, root));
+});
+
+test("binary tree should remove node with one child", t => {
+    t.plan(2);
+
+    const node11 = new TreeNode(11);
+    const node10 = new TreeNode(10, undefined, node11);
+
+    const node30 = new TreeNode(30);
+    const node40 = new TreeNode(40);
+    const node33 = new TreeNode(33, node30, node40);
+
+    const node25 = new TreeNode(25, node10, node33);
+
+    const node52 = new TreeNode(52);
+    const node61 = new TreeNode(61);
+    const node56 = new TreeNode(56, node52, node61);
+
+    const node82 = new TreeNode(82);
+    const node95 = new TreeNode(95);
+    const node89 = new TreeNode(89, node82, node95);
+
+    const node75 = new TreeNode(75, node56, node89);
+
+    const root = new TreeNode(50, node25, node75);
+
+    remove(10, root);
+
+    t.notOk(search(10, root));
+    t.ok(search(11, root));
+});
+
+test("binary tree should remove node with two children", t => {
+    t.plan(3);
+
+    const node4 = new TreeNode(4);
+    const node11 = new TreeNode(11);
+    const node10 = new TreeNode(10, node4, node11);
+
+    const node30 = new TreeNode(30);
+    const node40 = new TreeNode(40);
+    const node33 = new TreeNode(33, node30, node40);
+
+    const node25 = new TreeNode(25, node10, node33);
+
+    const node52 = new TreeNode(52);
+    const node61 = new TreeNode(61);
+    const node56 = new TreeNode(56, node52, node61);
+
+    const node82 = new TreeNode(82);
+    const node95 = new TreeNode(95);
+    const node89 = new TreeNode(89, node82, node95);
+
+    const node75 = new TreeNode(75, node56, node89);
+
+    const root = new TreeNode(50, node25, node75);
+
+    remove(10, root);
+
+    t.notOk(search(10, root));
+    t.ok(search(4, root));
+    t.ok(search(11, root));
+});
